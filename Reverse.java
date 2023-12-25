766093be97219763aaaf8344d5a6c41caddf3546
Romi Golan <romi.golan@post.runi.ac.il>
2023-12-25 13:45:28 +0200

    ddd

--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,30 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
-	public static void main (String[] args){
-		//// Put your code here
+	public static void main (String[] args)
+	{
+		String word = args[0];
+		int middle;
+		if(word.length() % 2 == 0) //this means that the length isn't exual
+			{
+				middle = (word.length() + 1) / 2;
+			}
+		else
+			{
+				middle = word.length() / 2;
+			}
+
+		char outLetter = word.charAt(middle);
+		System.out.println(outLetter);
+
+
+		int i = word.length()-1;
+
+		while(i >= 0)
+		{
+			char letter = word.charAt(i);
+			System.out.print(letter);
+			i--;
+		}
 	}
 }
