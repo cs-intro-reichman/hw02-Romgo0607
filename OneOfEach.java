292a1aed2d960984c03403bae332bbf2fbac4fdd
Romi Golan <romi.golan@post.runi.ac.il>
2023-12-25 19:34:23 +0200

    so so

--- OneOfEach.java
+++ OneOfEach.java
@@ -4,7 +4,32 @@
  *  to have children until they have at least one child of each gender.
  */
 public class OneOfEach {
-	public static void main (String[] args) {
-		//// Put your code here
+	public static void main (String[] args) 
+	{
+		int boyORgirl;
+		char guess;
+		int boys = 0;
+		int girls = 0;
+
+
+		while((girls < 2) && (boys < 2))
+		{
+			boyORgirl = (int)(Math.random() * 2);
+			
+			if(boyORgirl == 0)
+			{
+				guess = 'g';
+				girls++;
+			}
+			else
+			{
+				guess = 'b';
+				boys++;
+			}
+
+			System.out.print(" " + guess);
+		}
+		System.out.println();
+		System.out.println("You made it... and you now have " + (girls + boys)+ " children");
 	}
 }
