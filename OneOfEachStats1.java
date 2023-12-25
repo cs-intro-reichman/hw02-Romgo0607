2579a74fcd75dcc5f4e5942dfea5951f6b11ae29
Romi Golan <romi.golan@post.runi.ac.il>
2023-12-25 21:21:24 +0200

    i did it!

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -5,7 +5,87 @@
  * 	that determines how many families to simulate.
  */
 public class OneOfEachStats1 {
-	public static void main (String[] args) {
-		//// Put your code here
+	public static void main (String[] args) 
+	{
+		int families = Integer.parseInt(args[0]);
+		int i = 1;
+
+		int boyORgirl;
+		int boys = 0;
+		int girls = 0;
+
+		boolean enough = false;
+
+		int sumKidsAvrege = 0;
+
+		int TwoMembers = 0;
+		int ThreeMembers = 0;
+		int FourOrMoreMembers = 0;
+
+		while(i <= families)
+		{
+			while(enough == false)
+			{
+				boyORgirl = (int)(Math.random() * 2);
+				if(boyORgirl == 0)
+				{
+					System.out.print("g ");
+					girls++;
+				}
+				else
+				{
+					System.out.print("b ");
+					boys++;
+				}
+
+				if((boys > 0) && (girls > 0))
+				{
+					enough = true;
+					System.out.println();
+					System.out.println("Family number " + i + " has " + (boys + girls) + " kids");
+					sumKidsAvrege = sumKidsAvrege + boys + girls;
+					if((boys + girls) < 3)
+						{
+							TwoMembers++;
+						}
+						else
+						{
+							if((boys + girls) == 3)
+							{
+								ThreeMembers++;
+							}
+							else
+							{
+								FourOrMoreMembers++;
+							}
+						}
+				}
+			}
+			i++;
+			enough = false;
+			boys = 0;
+			girls = 0;
+		}
+		double FamForAvg = families;
+
+		System.out.println("Average: " + (sumKidsAvrege/FamForAvg) + " children to get at least one of each gender.");
+
+		System.out.println("Number of families with 2 children: " + TwoMembers);
+		System.out.println("Number of families with 3 children: " + ThreeMembers);
+		System.out.println("Number of families with 4 or more children: " + FourOrMoreMembers);
+
+		if((TwoMembers >= ThreeMembers) && (TwoMembers >= FourOrMoreMembers))
+			{
+				System.out.println("The most common number of children is 2.");
+			}
+			else
+			{
+				if(ThreeMembers >= FourOrMoreMembers)
+				{
+					System.out.println("The most common number of children is 3.");
+				}
+				else
+					System.out.println("The most common number of children is 4 or more.");
+			}
 	}
 }
